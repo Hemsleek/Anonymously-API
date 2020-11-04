d631ec935615e674857bc4f54da4a37228f5c668
hemsleek <asiyanbimubashir@gmail.com>
2020-11-04 15:06:48 +0100

    feat: database connection and route

--- src/index.js
+++ src/index.js
@@ -2,16 +2,31 @@ import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import { dbUrl, port } from './utils/config'
 
-const app= express()
+// routes
+import apiRouter from './routes'
+
+const app = express()
+
+mongoose.connect(dbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: true
+})
+
+const db = mongoose.connection
+db.on('error', console.log)
+db.once('open', () => console.log('DB connection successful'))
 
 app.disable('x-powered-by')
 app.use(express.json())
-app.use(morgan('dev'));
+app.use(morgan('dev'))
 app.use(cors())
 
-const port = 3030
+app.use('/api/v1', apiRouter)
 
-app.get('/anonymously',(req,res) => res.json('we are here😎'))
+app.get('/anonymously', (req, res) => res.json('we are here😎'))
 
-app.listen(port,()=>console.log(`app is running on port ${port}`))
+app.listen(port, () => console.log(`app is running on port ${port}`))
