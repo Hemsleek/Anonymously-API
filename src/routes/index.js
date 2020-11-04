e8b5f929ed625f6d576ef952b9376eb373f51272
hemsleek <asiyanbimubashir@gmail.com>
2020-11-04 18:01:38 +0100

    feat: login route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
 import { Router } from 'express'
-import { signup } from '../services'
+import { signup, login } from '../services'
 const router = Router()
 
 router.post('/signup', signup)
+router.post('/login', login)
 
 export default router
--- src/services/index.js
+++ src/services/index.js
@@ -4,15 +4,31 @@ export const signup = async (req, res) => {
   const { name, username, email, password } = req.body
 
   try {
+    const userExist = await User.countDocuments({ $or: [{ email }, { username }] })
+    if (userExist) return res.status(400).send('Username/Email already exist')
     const user = await new User({
       name, username, email, password
     }).save()
-
     res.json({
       message: 'signup successful',
       payload: user
     })
   } catch (error) {
-    res.status(500).send(`internal server error${error}`)
+    res.status(500).send('internal server error')
+  }
+}
+
+export const login = async (req, res) => {
+  const { username, email, password } = req.body
+  try {
+    const user = await User.findOne({ $or: [{ email }, { username }] })
+    if (!user) return res.status(404).send('incorrect login credentials,please try again')
+    if (user.password !== password) return res.status(404).send('incorrect login credentials,please try again')
+    res.json({
+      message: 'Login successful',
+      payload: user
+    })
+  } catch (error) {
+    res.status(500).send('internal server error')
   }
 }
